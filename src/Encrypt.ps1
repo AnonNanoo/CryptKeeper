a7dfa0d82ed327572bdaf531fbf66c3b8cf1836e
nano <df@proconsis.ch>
2024-11-20 17:48:53 +0100

    animated startup

--- src/Encrypt.ps1
+++ src/Encrypt.ps1
@@ -75,23 +75,34 @@ function Main {
 
 function logo {
     # This function prints the logo and welcome message to the console.
-    log -logtype 1 -logMessage "Log: Logo printed to console"
     clear-host
-    write-host " 
-   ______                 __  __ __                         
-  / ____/______  ______  / /_/ //_/__  ___  ____  ___  _____
- / /   / ___/ / / / __ \/ __/ ,< / _ \/ _ \/ __ \/ _ \/ ___/
-/ /___/ /  / /_/ / /_/ / /_/ /| /  __/  __/ /_/ /  __/ /    
-\____/_/   \__, / .___/\__/_/ |_\___/\___/ .___/\___/_/     
-          /____/_/                      /_/                                                                                                          
-                                                  ($ProjectVersion)
-===========================================================" -ForegroundColor blue
+    Write-Host "   ______                 __  __ __                         " 
+    Start-Sleep -Seconds 0.3
+    Write-Host "  / ____/______  ______  / /_/ //_/__  ___  ____  ___  _____" 
+    Start-Sleep -Seconds 0.3
+    Write-Host " / /   / ___/ / / / __ \/ __/ ,< / _ \/ _ \/ __ \/ _ \/ ___/" 
+    Start-Sleep -Seconds 0.3
+    Write-Host "/ /___/ /  / /_/ / /_/ / /_/ /| /  __/  __/ /_/ /  __/ /    " 
+    Start-Sleep -Seconds 0.3
+    Write-Host "\____/_/   \__, / .___/\__/_/ |_\___/\___/ .___/\___/_/     " 
+    Start-Sleep -Seconds 0.3
+    Write-Host "          /____/_/                      /_/                 "
+
+    Write-Host "                                                ($ProjectVersion)"
+    #Write-Host "===========================================================" -ForegroundColor blue
+    [int] $i = 0
+    do{
+        $i++
+        Start-Sleep -Milliseconds 5
+        Write-Host "=" -NoNewline -ForegroundColor blue
+    }until ($i -eq 60)
     Write-Host "`nWelcome to CryptKeeper!`n"
     Write-Host "Booting CryptKeeper" -NoNewline
+
     [int] $i = 0
     do {
         $i++
-        Start-Sleep -Seconds 0.5
+        Start-Sleep -Seconds 1
         Write-Host "." -NoNewline
     }until ($i -eq 3)
     Write-Host "`n"
@@ -99,6 +110,58 @@ function logo {
 
 
 function menu{
+    do {
+        log -logtype 1 -logMessage "Log: Menu opened"
+        clear-host
+        Write-Host "`n==========================================="
+        Write-Host "| $ProjectName (ver.$ProjectVersion)                  |"
+        Write-Host "|_________________________________________|"
+        Write-Host "| (1) Encrypt now                         |"
+        Write-Host "|                                         |"
+        Write-Host "| (99) Exit                               |"
+        Write-Host "|_________________________________________|`n"
+        $menuinput = read-host "Please select an option"
+        switch ( $menuinput ) {
+            1 {
+                log -logtype 1 -logMessage "Log: Sync started"
+                PreSync
+            }
+
+            95 {
+                Write-Host "READ BEFORE YOU ENTER`nYou are about to remove all information.`n`nIf you restart the script, you will be asked to reconfigure the settings and the script will automatically create the resource folders again." -ForegroundColor Red
+                $u_sure = Read-Host "`nAre you sure? (y/n)"
+                if ( $u_sure -eq "y" ) {
+                    try {
+                        Remove-Item -Path $Tempfolder -Recurse -Force
+                    }
+                    catch {
+                        Write-Host "Error: Removing the Tempfolder failed. Going back to Menu`nYou try to remove the folder manually. Path: $Tempfolder" -ForegroundColor Red
+                        Start-Sleep -Seconds 4
+                        menu
+                    }
+                    Write-Host "`nAll Information removed. Exiting in 3 seconds..." -ForegroundColor Green
+                    Start-Sleep -Seconds 3
+                    exit
+                }
+                else {
+                    Write-Host "`nAborted. Going back to Menu in 1 second..." -ForegroundColor Red
+                    Start-Sleep -Seconds 1
+                }
+            }
+            99 {
+                Write-Host "Bye!"
+                Start-Sleep -Seconds 1
+                exit
+            }
+            default {
+                log -logtype 1 -logMessage "Log: Invalid input in Menu"
+                write-host "Invalid input. Try again"
+                Write-host "To exit, enter 99" -ForegroundColor Red
+                Start-Sleep -Milliseconds 1500
+            }
+        }
+        $menuinput = 0
+    }until (0 -eq 1)
 
 }
 logo

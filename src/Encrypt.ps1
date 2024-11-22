2c33051d1de121a7a0870d1f5468c4019fbe2615
nano <df@proconsis.ch>
2024-11-22 07:57:07 +0100

    added variables, added source code option to menu

--- src/Encrypt.ps1
+++ src/Encrypt.ps1
@@ -1,35 +1,38 @@
 
+# Variables
+[int] $menuinput # User input for menu selection
+[string] $syncoptions # Sync options
 
+# Project related variables
 [string] $ProjectName = "CryptKeeper"
 [string] $ProjectVersion = "0.0.0.1"
 [string] $Tempfolder = [Environment]::GetFolderPath( 'LocalApplicationData' ) + "\CryptKeeper"
 
+# Source and destination paths for file synchronization
+[string] $inputPath = ""
+[string] $outputPath = ""
 
+# Password for encryption
+[string]$password = ""
 
-$colors = @{
-    "red"      = "`e[31m"  # Red
-    "green"    = "`e[32m"  # Green
-    "yellow"   = "`e[33m"  # Yellow
-    "blue"     = "`e[34m"  # Blue
-    "magenta"  = "`e[35m"  # Magenta
-    "cyan"     = "`e[36m"  # Cyan
-    "white"    = "`e[37m"  # White
-    "reset"    = "`e[0m"   # Reset color
-}
+# File paths for settings and logs
+[string] $settingsFilePath = "$Tempfolder\Settings.dll"
+[string] $ErrorLogFilePath = "$Tempfolder\ErrorLog.log"
+[string] $logFilePath = "$Tempfolder\FileSync.log"
+[string] $logMessage
+[int] $corrupted
 
-
-
-function Main {
+function encrypt {
     Clear-Host
 
      # Prompt user for inputs
      Write-Host "Enter the path to the input file (file to be encrypted)" -ForegroundColor Yellow
-     [string]$inputPath = Read-Host
+     $inputPath = Read-Host
      Write-Host "`nEnter the path to the output file (where the encrypted file will be saved)" -ForegroundColor Yellow
-     [string]$outputPath = Read-Host
+     $outputPath = Read-Host
  
      Write-Host "`nEnter the password for encryption" -ForegroundColor Cyan
-     [string]$password = Read-Host
+     $password = Read-Host
  
     $inputExists = Test-Path $inputPath
     $outputExists = Test-Path $outputPath
@@ -50,10 +53,10 @@ function Main {
         Write-Host "Press any key to return..." -ForegroundColor Yellow
         read-host
 
-        $inputExists = $null
-        $outputExists = $null
+        $inputExists = ""
+        $outputExists = ""
         $passwordExists = ""
-        main
+        encrypt
         return
     }
 
@@ -75,28 +78,30 @@ function Main {
 
 function logo {
     # This function prints the logo and welcome message to the console.
+    [double] $time = 0.15
     clear-host
     Write-Host "   ______                 __  __ __                         " 
-    Start-Sleep -Seconds 0.3
+    Start-Sleep -Seconds $time
     Write-Host "  / ____/______  ______  / /_/ //_/__  ___  ____  ___  _____" 
-    Start-Sleep -Seconds 0.3
+    Start-Sleep -Seconds $time
     Write-Host " / /   / ___/ / / / __ \/ __/ ,< / _ \/ _ \/ __ \/ _ \/ ___/" 
-    Start-Sleep -Seconds 0.3
+    Start-Sleep -Seconds $time
     Write-Host "/ /___/ /  / /_/ / /_/ / /_/ /| /  __/  __/ /_/ /  __/ /    " 
-    Start-Sleep -Seconds 0.3
+    Start-Sleep -Seconds $time
     Write-Host "\____/_/   \__, / .___/\__/_/ |_\___/\___/ .___/\___/_/     " 
-    Start-Sleep -Seconds 0.3
+    Start-Sleep -Seconds $time
     Write-Host "          /____/_/                      /_/                 "
-
-    Write-Host "                                                ($ProjectVersion)"
+    Start-Sleep -Seconds $time
+    Write-Host "                                                  ($ProjectVersion)"
     #Write-Host "===========================================================" -ForegroundColor blue
+
     [int] $i = 0
     do{
         $i++
         Start-Sleep -Milliseconds 5
         Write-Host "=" -NoNewline -ForegroundColor blue
     }until ($i -eq 60)
-    Write-Host "`nWelcome to CryptKeeper!`n"
+    Write-Host "`nWelcome to CryptKeeper!`n" 
     Write-Host "Booting CryptKeeper" -NoNewline
 
     [int] $i = 0
@@ -111,42 +116,26 @@ function logo {
 
 function menu{
     do {
-        log -logtype 1 -logMessage "Log: Menu opened"
         clear-host
         Write-Host "`n==========================================="
-        Write-Host "| $ProjectName (ver.$ProjectVersion)                  |"
+        Write-Host "| $ProjectName (ver.$ProjectVersion)               |"
         Write-Host "|_________________________________________|"
         Write-Host "| (1) Encrypt now                         |"
+        Write-Host "| (2) Open Log                            |"
+        Write-Host "| (3) Sourcecode                          |"
         Write-Host "|                                         |"
         Write-Host "| (99) Exit                               |"
         Write-Host "|_________________________________________|`n"
         $menuinput = read-host "Please select an option"
         switch ( $menuinput ) {
             1 {
-                log -logtype 1 -logMessage "Log: Sync started"
-                PreSync
+                
             }
-
-            95 {
-                Write-Host "READ BEFORE YOU ENTER`nYou are about to remove all information.`n`nIf you restart the script, you will be asked to reconfigure the settings and the script will automatically create the resource folders again." -ForegroundColor Red
-                $u_sure = Read-Host "`nAre you sure? (y/n)"
-                if ( $u_sure -eq "y" ) {
-                    try {
-                        Remove-Item -Path $Tempfolder -Recurse -Force
-                    }
-                    catch {
-                        Write-Host "Error: Removing the Tempfolder failed. Going back to Menu`nYou try to remove the folder manually. Path: $Tempfolder" -ForegroundColor Red
-                        Start-Sleep -Seconds 4
-                        menu
-                    }
-                    Write-Host "`nAll Information removed. Exiting in 3 seconds..." -ForegroundColor Green
-                    Start-Sleep -Seconds 3
-                    exit
-                }
-                else {
-                    Write-Host "`nAborted. Going back to Menu in 1 second..." -ForegroundColor Red
-                    Start-Sleep -Seconds 1
-                }
+            2{
+                printLog
+            }
+            3{
+                printSourceCode
             }
             99 {
                 Write-Host "Bye!"
@@ -154,8 +143,7 @@ function menu{
                 exit
             }
             default {
-                log -logtype 1 -logMessage "Log: Invalid input in Menu"
-                write-host "Invalid input. Try again"
+                Write-host "Invalid input. Try again"
                 Write-host "To exit, enter 99" -ForegroundColor Red
                 Start-Sleep -Milliseconds 1500
             }
@@ -164,5 +152,45 @@ function menu{
     }until (0 -eq 1)
 
 }
+
+function printSourceCode {
+    clear-host
+    Write-Host "Source code for CryptKeeper" -ForegroundColor Yellow
+    Write-Host "=============================" -ForegroundColor Yellow
+    Write-Host "`n"
+    Write-Host "https://github.com/AnonNanoo/CryptKeeper/src/Encrypt.ps1"
+    Write-Host "`n"
+    Write-Host "Press any key to return to the menu..." -ForegroundColor Yellow
+    read-host
+    menu
+}
+
+
+function log {
+    # This function logs messages to the log file or error log file based on the given log type.
+    param(
+        [int]$logtype,
+        [string]$logMessage
+    )
+    try {
+        if ( (test-path -path $logFilePath ) -and $logtype -eq 1 ) {    # 1 is for normal log
+            [string] $timestamp = Get-Date -Format  "dd-MM-yyyy HH:mm.sss" 
+            $logMessage = $logMessage + " at $timestamp"
+            $logMessage | Out-File -FilePath $logFilePath -Append -Encoding utf8
+        }
+        elseif ((test-path -path $ErrorLogFilepath ) -and $logtype -eq 2) {   # 2 is for error log
+            [string] $timestamp = Get-Date -Format  "dd-MM-yyyy HH:mm.sss"
+            $logMessage = $logMessage + " at $timestamp"
+            $logMessage | Out-File -FilePath $ErrorLogFilePath -Append -Encoding utf8
+        }
+    }
+    catch {
+        return
+    }
+}
+
+
 logo
+menu
+
 
